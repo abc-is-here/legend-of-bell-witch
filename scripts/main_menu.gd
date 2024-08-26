a15cf4c4b973b9f2c61d0e72d82107c9a8a08bc2
abc <abhaygupta.hsj@gmail.com>
2024-08-26 19:56:15 +0530

    Papers work!! As well as main menu!

---
+++ scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func play():
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	
+func quit():
+	get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	play()
+
+
+
+func _on_button_3_pressed() -> void:
+	quit()
