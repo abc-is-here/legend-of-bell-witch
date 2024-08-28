a05c805fabe3bb0677d2d84f25e8bd062629e6dd
abc <abhaygupta.hsj@gmail.com>
2024-08-28 16:31:29 +0530

    Fixed Unlimited battery glitch, added light flicker

--- battery.gd
+++ battery.gd
@@ -2,9 +2,16 @@ extends StaticBody3D
 
 var flashlight_energy
 var energy_add_amount
+var pickUp_sound
+var flashlight
 
 func _ready() -> void:
+	flashlight = get_node("/root/"+get_tree().current_scene.name+"/Player/head/flashlight")
+	pickUp_sound = get_node("/root/" + get_tree().current_scene.name + "/sounds/ObjPickUp")
 	flashlight_energy = get_node("/root/" + get_tree().current_scene.name + "/UI/flashlight_stuff/flashlightSlider")
 
 func interact():
-	flashlight_energy.value += energy_add_amount
+	if flashlight.picked_up == true:
+		flashlight_energy.value += energy_add_amount
+		pickUp_sound.play()
+		queue_free()
---
+++ scripts/battery.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@export var min_time: float
+@export var max_time: float
+@export var min_flash_time: float
+@export var max_flash_time: float
+@export var loop_flashing: bool
+
+var loop = true
+
+func _process(delta: float) -> void:
+	if loop:
+		loop = false
+		var rng = RandomNumberGenerator.new()
+		var rand = rng.randf_range(min_time, max_time)
+		await get_tree().create_timer(rand, false).timeout
+		if loop_flashing:
+			$AnimationPlayer.get_animation("flashing_light").loop = true
+		$AnimationPlayer.play("flashing_light")
+		if loop_flashing == false:
+			loop = true
+		if loop_flashing == true:
+			var rand2 = rng.randf_range(min_flash_time, max_flash_time)
+			await get_tree().create_timer(rand2, false).timeout
+			$AnimationPlayer.get_animation("flashing_light").loop = false
+			loop = true
